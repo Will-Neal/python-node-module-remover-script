333d3246bbe00685eecc3d48d42a83db08a78e3b
Will Neal <willwneal@gmail.com>
2022-07-08 10:07:34 -0400

    Added ability to delete package-locks

--- nm_remove.py
+++ nm_remove.py
@@ -1,7 +1,21 @@
 import os
+from re import S
 
 def init(): 
-    sendIt = int(input('See node_modules first? \n 1 - Yes, please!  \n 2 - No, send it \n 3 - What have I done?? Get me out of here \n'))
+    def packageLocks():
+        lockThePackage = int(input('Do you want to get rid of the package-lock.jsons too? \n 1 - No, I thought this was just for node_modules, get me outta here!! \n 2 - Yes, delete the pesky package-locks \n'))
+
+        if lockThePackage == 1:
+            print('Alright see ya later')
+
+        if lockThePackage == 2:
+            print('Yipee ki-yay!')
+            os.system("find . -name 'package-lock.json' -type d -prune")
+            os.system("find . -name 'package-lock.json' -type d -prune -exec rm -rf '{}' +")
+            print("EZPZ package-lock's are no more")
+
+
+    sendIt = int(input('See node_modules first? \n 1 - Yes, please!  \n 2 - No, send it \n 3 - What have I done?? Get me out of here!! \n'))
 
     if sendIt == 1:
         print("You didn't send it :(")
@@ -14,7 +28,7 @@ def init():
         # '{}' is a placeholder which find replaces with the file path it found, while + tells find to append all the file paths to a single command, rather than running rm for each
         os.system("find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +")
 
-        print("node_module pruning complete!!")
+        print("no more node_modules!!")
 
     elif sendIt ==3:
         return
