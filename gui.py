a2a88732a84effe26aa52cf9f8429b6ffb412dca
Will Neal <willwneal@gmail.com>
2022-07-24 12:38:06 -0400

    Added buttons for functions

--- gui.py
+++ gui.py
@@ -11,10 +11,19 @@ canvas.pack()
 frame = tk.Frame(root, bg='#e6ffff')
 frame.place(relx=0.1,rely=0.1, relwidth=0.8, relheight=0.8)
 
-button = tk.Button(frame, bg='#d9d9d9', text="See Node_Modules")
-button.pack()
+label = tk.Label(root, bg='#999999', text='No_Modules')
+label.place(relx=0.4, rely=0, relwidth=.2, relheight=.1)
+ 
+seeButton = tk.Button(frame, bg='#d9d9d9', text="See Node_Modules")
+seeButton.place(relx=0.2, rely=0.25, relwidth=.2, relheight=.05)
 
-button = tk.Button(frame, bg='#d9d9d9', text="Delete Node_Modules")
-button.pack()
+deleteButton = tk.Button(frame, bg='#d9d9d9', text="Delete Node_Modules")
+deleteButton.place(relx=0.6, rely=0.25, relwidth=.2, relheight=.05)
+
+seeJsonButton = tk.Button(frame, bg='#d9d9d9', text="See Package-Lock.jsons")
+seeJsonButton.place(relx=0.2, rely=0.35, relwidth=.2, relheight=.05)
+
+deleteJsonButton = tk.Button(frame, bg='#d9d9d9', text="Delete Node_Modules")
+deleteJsonButton.place(relx=0.6, rely=0.35, relwidth=.2, relheight=.05)
 
 root.mainloop()
