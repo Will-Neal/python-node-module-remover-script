1558bcb48e2e0c0fd5c7aad8382a5e25668fdd0f
Will Neal <willwneal@gmail.com>
2022-07-30 21:00:33 -0400

    Touched up tkinter gui

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
-HEIGHT = 700
-WIDTH = 800
+HEIGHT = 450
+WIDTH = 600
 
 root = tk.Tk()
 
@@ -15,15 +15,15 @@ label = tk.Label(frame, bg='#bb86fc', text='No_Mo')
 label.place(relx=0.4, rely=.05, relwidth=.2, relheight=.1)
  
 seeButton = tk.Button(frame, bg='#03dac6', text="See Node_Modules")
-seeButton.place(relx=0.25, rely=0.2, relwidth=.2, relheight=.09)
+seeButton.place(relx=0.2, rely=0.2, relwidth=.3, relheight=.09)
 
 deleteButton = tk.Button(frame, bg='#cf6679', text="Delete Node_Modules")
-deleteButton.place(relx=0.55, rely=0.2, relwidth=.2, relheight=.09)
+deleteButton.place(relx=0.5, rely=0.2, relwidth=.3, relheight=.09)
 
 seeJsonButton = tk.Button(frame, bg='#03dac6', text="See Package-Lock.jsons")
-seeJsonButton.place(relx=0.25, rely=0.35, relwidth=.2, relheight=.09)
+seeJsonButton.place(relx=0.2, rely=0.35, relwidth=.3, relheight=.09)
 
 deleteJsonButton = tk.Button(frame, bg='#cf6679', text="Delete Node_Modules")
-deleteJsonButton.place(relx=0.55, rely=0.35, relwidth=.2, relheight=.09)
+deleteJsonButton.place(relx=0.5, rely=0.35, relwidth=.3, relheight=.09)
 
 root.mainloop()
