a3761fe9dd23bc1d8f94c9911f630c8fdc5956d9
Will Neal <willwneal@gmail.com>
2022-07-30 21:19:04 -0400

    Working buttons

--- gui.py
+++ gui.py
@@ -3,6 +3,20 @@ import tkinter as tk
 HEIGHT = 450
 WIDTH = 600
 
+def seeNM():
+    print('See Node Module Function')
+
+def deleteNM():
+    print('Delete Node Module function')
+
+def seePJ():
+    print('See Package.Json file')
+
+def deletePJ():
+    print('Delete Package.json file')
+
+
+
 root = tk.Tk()
 
 canvas = tk.Canvas(root, bg='#19194d', height=HEIGHT, width=WIDTH)
@@ -14,16 +28,16 @@ frame.place(relx=0.1,rely=0.1, relwidth=0.8, relheight=0.8)
 label = tk.Label(frame, bg='#bb86fc', text='No_Mo')
 label.place(relx=0.4, rely=.05, relwidth=.2, relheight=.1)
  
-seeButton = tk.Button(frame, bg='#03dac6', text="See Node_Modules")
+seeButton = tk.Button(frame, bg='#03dac6', text="See Node_Modules", command=seeNM)
 seeButton.place(relx=0.2, rely=0.2, relwidth=.3, relheight=.09)
 
-deleteButton = tk.Button(frame, bg='#cf6679', text="Delete Node_Modules")
+deleteButton = tk.Button(frame, bg='#cf6679', text="Delete Node_Modules", command=deleteNM)
 deleteButton.place(relx=0.5, rely=0.2, relwidth=.3, relheight=.09)
 
-seeJsonButton = tk.Button(frame, bg='#03dac6', text="See Package-Lock.jsons")
+seeJsonButton = tk.Button(frame, bg='#03dac6', text="See Package-Lock.jsons", command=seePJ)
 seeJsonButton.place(relx=0.2, rely=0.35, relwidth=.3, relheight=.09)
 
-deleteJsonButton = tk.Button(frame, bg='#cf6679', text="Delete Node_Modules")
+deleteJsonButton = tk.Button(frame, bg='#cf6679', text="Delete Package.Json", command=deletePJ)
 deleteJsonButton.place(relx=0.5, rely=0.35, relwidth=.3, relheight=.09)
 
 root.mainloop()
